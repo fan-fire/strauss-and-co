cb8cab9bca7794d5ac9c135d54766e0fa2f7fcf3
Louwrens <louwjlabuschagne@gmail.com>
2023-05-18 06:58:11 +0200

    perf: :zap: using optimizer to reduce contract size

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,9 +2,21 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-contract-sizer";
 import 'solidity-coverage';
-
+import 'hardhat-test-utils';
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.18',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ]
+  },
   contractSizer: {
     alphaSort: true,
     disambiguatePaths: false,
