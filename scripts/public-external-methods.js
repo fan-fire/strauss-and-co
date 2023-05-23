d0f41e3a9416f3bdb687537b8e3a6b9a517a5fc0
Louwrens <louwjlabuschagne@gmail.com>
2023-05-23 04:50:18 +0200

    a script to get all public and external methods

---
+++ scripts/public-external-methods.js
@@ -0,0 +1,24 @@
+function getContractFunctions() {
+    const artifact = require('../artifacts/contracts/Basket.sol/Basket.json');
+  
+    const abi = artifact.abi;
+    // console.log(`ABI: ${JSON.stringify(abi, null, 2)}`)
+  
+    console.log('Public and External Functions:');
+    abi.forEach((element) => {
+      // console.log(`Element: ${JSON.stringify(element, null, 2)}`)
+      if (
+        element.type === 'function' && 
+        (element.stateMutability !== 'view' && element.stateMutability !== 'pure')
+      ) {
+        // create a string that looks like a function definition for a markdown table
+        let prettyFunction = `${element.name}(${element.inputs.map((input) => `${input.type} ${input.name}`).join(', ')})`;
+        prettyFunction += element.outputs.length > 0 ? ` returns (${element.outputs.map((output) => `${output.type} ${output.name}`).join(', ')})` : '';
+        console.log(prettyFunction);
+        
+      }
+    });
+  }
+  
+  getContractFunctions();
+  
